dee0b62ece11483808c1e3eae41256933e417644
oscartcy <tang_co@yahoo.com.hk>
2014-09-20 16:23:19 +0800

    add -i flag to grep

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'Start Running'
-curl https://reserve.cdn-apple.com/HK/en_HK/reserve/iPhone/availability.json | grep 'true'
+curl https://reserve.cdn-apple.com/HK/en_HK/reserve/iPhone/availability.json | grep -i 'true'
 while [ $? -ne 0 ];
 do
-curl https://reserve.cdn-apple.com/HK/en_HK/reserve/iPhone/availability.json | grep 'true'
+curl https://reserve.cdn-apple.com/HK/en_HK/reserve/iPhone/availability.json | grep -i 'true'
 done
 
 echo 'Available!!!!!'
